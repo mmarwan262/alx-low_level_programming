e1e1ea2e914d30eeca8db9058c565c0ffe9ab686
mmarwan262 <mmarwan199898@gmail.com>
2023-10-11 12:38:39 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line.
-*
+ * print_alphabet - utilizes on the_putcher function to print the alphabet a - z
+ *
 */
 
 void print_alphabet(void)
