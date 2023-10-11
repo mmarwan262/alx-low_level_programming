6c1ea5ed53e90e3cc2a8822f757595f5d74db9ba
mmarwan262 <mmarwan199898@gmail.com>
2023-10-11 12:07:16 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,26 +4,29 @@
  * main - Entry point of the program
  *
  * Description: This program prints all the numbers of base 16 in lowercase,
- * followed by a new line. It uses only three putchar calls.
+ * followed by a new line. It uses only three putchar calls and does not have
+ * trailing whitespace.
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-char digit, lowercaseHex;
-    
-for (digit = '0'; digit <= '9'; digit++)
-{
-putchar(digit);
-}
-    
-for (lowercaseHex = 'a'; lowercaseHex <= 'f'; lowercaseHex++)
-{
-putchar(lowercaseHex);
-}
-    
-putchar('\n');
+int num = 0;
 
+while (num < 10)
+{
+putchar(num + '0');
+num++;
+}
+
+num = 'a';
+while (num <= 'f')
+{
+putchar(num);
+num++;
+}
+
+putchar('\n');
 return (0);
 }
