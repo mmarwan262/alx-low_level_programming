48873720bf5019cf7fa8ff612a48743217f0e8f1
mmarwan262 <mmarwan199898@gmail.com>
2023-10-19 11:08:44 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - Changes all lowercase letters in a string to uppercase.
+ * @str: The input string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
+
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+}
+
+return (str);
+}
