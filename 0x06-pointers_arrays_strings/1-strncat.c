a5cb497b1020360d9165a28f9e3c4a71bd84e668
mmarwan262 <mmarwan199898@gmail.com>
2023-10-19 10:47:41 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,31 @@
-#include "main.h"
-
 /**
- * _strncat - a function that concatenates two strings.
+ * _strncat - Concatenates two strings, using at most n bytes from src.
+ * @dest: The destination string to which src will be concatenated.
+ * @src: The source string to concatenate to dest.
+ * @n: The maximum number of bytes from src to concatenate.
  *
- * @dest: pointer to destination input
- * @src: pointer to source input
- * @n: most nnumber of bytes from @src
- *
- * Return: @dest
-*/
+ * Return: A pointer to the resulting string (dest).
+ */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, const char *src, int n)
 {
-int c, i;
+int dest_len = 0;
+int i;
 
-c = 0;
+/* Find the end of the destination string */
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
 
-/*find size of dest array*/
-while (dest[c])
-c++;
+/* Concatenate src to dest, using at most n bytes */
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[dest_len + i] = src[i];
+}
 
-/**
-* src does not need to be null terminated
-* it it contains n or more bytes
-*/
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[c + i] = src[i];
-/*null terminates dest*/
-dest [c + i] = '\0';
+/* Ensure the resulting string is null-terminated */
+dest[dest_len + i] = '\0';
 
 return (dest);
-} 
+}
