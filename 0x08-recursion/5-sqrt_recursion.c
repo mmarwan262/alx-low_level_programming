3181cc3ab4ca886b3a207cbf63ac3585383e951b
mmarwan262 <mmarwan199898@gmail.com>
2023-10-25 08:04:23 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,23 @@
 #include "main.h"
 
 /**
+ * square - find natural square root
+ * @n: int
+ * @val: square root
+ *
+ * Description: This function recursively searches for the natural square root
+ * of the given number 'n' using a helper value 'val'.
+ *
+ * Return: int
+ */
+int square(int n, int val);
+
+/**
  * _sqrt_recursion - find natural square root
  * @n: int
+ *
+ * Description: This function calculates the natural square root of the given
+ * number 'n' using recursion.
  *
  * Return: int
  */
@@ -15,6 +30,9 @@ return (square(n, 1));
  * square - find natural square root
  * @n: int
  * @val: square root
+ *
+ * Description: This function recursively searches for the natural square root
+ * of the given number 'n' using a helper value 'val'.
  *
  * Return: int
  */
