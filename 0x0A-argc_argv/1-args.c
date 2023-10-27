34da3b8fe827dcaa3cf8eabf3f128b4106ba45a3
mmarwan262 <mmarwan199898@gmail.com>
2023-10-27 08:06:18 -0700

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0
+*/
+
 int main(int argc, char *argv[])
 {
 /* Suppress unused variable warning for argv */
