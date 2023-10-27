8cf6315f350056adb92288ba860efa80f0ed466e
mmarwan262 <mmarwan199898@gmail.com>
2023-10-27 08:21:13 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,26 +2,25 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
+ * main-print sum of 2 numbers.
+ * @argc:number of commandline arguments.
+ * @argv:pointer to an array containing the command-line arguments.
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success, non-zero-fail.
  */
 int main(int argc, char *argv[])
 {
-if (argc != 3)
+int sum;
+
+| if (argc == 3)
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", sum);
+}
+else
 {
 printf("Error\n");
 return (1);
 }
-
-int num1 = atoi(argv[1]); /* Convert the first argument to an integer */
-int num2 = atoi(argv[2]); /* Convert the second argument to an integer */
-
-int result = num1 * num2; /* Multiply the two numbers */
-
-printf("%d\n", result); /* Print the result followed by a new line */
-
 return (0);
 }
