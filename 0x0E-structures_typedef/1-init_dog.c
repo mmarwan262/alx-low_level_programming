9bd0ffec5da7f3c060b93018badec222d9d59708
mmarwan262 <mmarwan199898@gmail.com>
2023-11-05 23:31:15 -0800

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,8 @@
  *
  * Return: void.
 */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 {
 if (d)
 {
