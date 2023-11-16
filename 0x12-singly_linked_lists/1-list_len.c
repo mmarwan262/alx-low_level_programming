a02370f11ebc55a758e6a323991c9e4c9b04aa1a
mmarwan262 <mmarwan199898@gmail.com>
2023-11-16 11:02:37 -0800

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,20 @@
-#include <stddef.h>
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a linked list_t list.
- * @h: Pointer to the head of the list.
+ * list_len - determines length of linked list
+ * @h: Pointer to first node
  *
- * Return: The number of nodes.
+ * Return: size of list
  */
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
+size_t i = 0;
 
-while (h != NULL)
+while (h)
 {
-count++;
 h = h->next;
+i++
 }
 
-return (count);
+return (i);
 }
