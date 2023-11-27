7658c15b0f28162741d6ad8ab5cd6e48837d0f0b
mmarwan262 <mmarwan199898@gmail.com>
2023-11-27 04:27:40 -0800

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,25 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * read_textfile - Reads a text file and prints it
  * @filename: Name of the file to read
- * @letters: Number of letters to read and print
+ * @letters: Number of bytes to read
  *
- * Return: The actual number of letters read and printed, or 0 on failure
+ * Return: The number of bytes read/printed
 */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters);
 {
-int fd, read_chars, write_chars;
-char *buffer;
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
 
-if (filename == NULL)
-return (0);
-
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-close(fd);
-return (0);
-}
-
-read_chars = read(fd, buffer, letters);
-close(fd);
-
-if (read_chars == -1)
-{
-free(buffer);
-return (0);
-}
-
-write_chars = write(STDOUT_FILENO, buffer, read_chars);
-free(buffer);
-
-if (write_chars != read_chars)
-return (0);
-
-return (read_chars);
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
 }
