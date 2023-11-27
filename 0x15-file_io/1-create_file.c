1fdad152ac1e6fc489534e014e76d647b41e860b
mmarwan262 <mmarwan199898@gmail.com>
2023-11-27 05:19:23 -0800

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,41 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with specified permissions and writes content
- * @filename: Name of the file to create
- * @text_content: Content to write to the file
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: 1 on success, -1 on failure
- */
+ * Return: integer length of string
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return(0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-int fd, write_chars;
-mode_t file_permissions = S_IRUSR | S_IWUSR; /* rw------- */
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
-if (filename == NULL)
-return (-1);
-
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
-if (fd == -1)
-return (-1);
-
-if (text_content != NULL)
-{
-write_chars = write(fd, text_content, strlen(text_content));
-if (write_chars == -1)
-{
-close(fd);
-return (-1);
-}
-}
-
-close(fd);
-return (1);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
 }
