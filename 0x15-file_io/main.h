c4ac824facbede843e80817a9fcf40587d5e1524
mmarwan262 <mmarwan199898@gmail.com>
2023-11-27 05:23:37 -0800

    task1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int_strlen(char *s);
 
 #endif
